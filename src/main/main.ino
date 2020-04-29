c60752efbbea1d7334db85cdf19cc507854b5b61
Vladyslav Knysh <vladyslav.knysh@agro-online.com>
2020-04-29 23:39:20 +0300

    added monitor

--- src/main/main.ino
+++ src/main/main.ino
@@ -2,34 +2,38 @@
 #include <Wire.h>
 #include "lightsensor.h"
 #include "light_calc.h"
-
-
-//BH1750 lightMeter;
+#include "monitor.h"
 
 float lux = 0;
 float ev = 0;
 
 void setup() {
-  Serial.begin(9600);
-  lightsensorSetup();
+    Serial.begin(9600);
+    lightsensorSetup();
+    monitorSetup();
 }
 
 void loop() {
-  lux = getlux();
-  ev = luxToEv(lux);
-  print(" lux: ", lux);
-  print(" ev: ", ev);
-  delay(1000);
+    lux = getlux();
+    ev = luxToEv(lux);
+    print(" lux: ", lux);
+    print(" ev: ", ev);
+    showOneDecimalPrecision(ev);
+   // Expect: ev with one decimal precision
+    delay(1000);
 }
 
 void print(String key, String val) {
-  Serial.print(key);
-  Serial.print(val);
-  Serial.println(";");
+    Serial.print(key);
+    Serial.print(val);
+    Serial.println(";");
 }
 void print(String key, int val) {
-  print(key, String(val));
+    print(key, String(val));
 }
 void print(String key, float val) {
-  print(key, String(val));
+    print(key, String(val));
+}
+void print(String key, double val) {
+    print(key, String(val));
 }
---
+++ src/main/monitor.h
@@ -0,0 +1,7 @@
+#ifndef HEADER_MONITOR
+    #define HEADER_MONITOR
+
+    //Prototype for helper_function found in HelperFunctions.cpp
+    void monitorSetup( void );
+    void showOneDecimalPrecision( int num );
+#endif
